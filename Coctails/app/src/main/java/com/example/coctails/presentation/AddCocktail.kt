54774fa787bcaeb32987930eafdcac75f0365703
Lobiofrom <124072945+Lobiofrom@users.noreply.github.com>
2023-10-12 21:08:47 +0300

    n

--- Coctails/app/src/main/java/com/example/coctails/presentation/AddCocktail.kt
+++ Coctails/app/src/main/java/com/example/coctails/presentation/AddCocktail.kt
@@ -17,10 +17,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -99,7 +101,9 @@ fun AddCocktail(
             onIconClicked,
             modifier = Modifier
                 .align(Alignment.Start)
-                .padding(16.dp)
+                .padding(6.dp)
+                .size(38.dp)
+                .background(color = Color.White, shape = CircleShape)
         )
         Column(
             modifier = Modifier
