f5086a63ed4aa8a3843d06d310b21e772d793666
Lobiofrom <124072945+Lobiofrom@users.noreply.github.com>
2023-10-15 11:09:24 +0300

    т

--- kinopoisk Test/app/src/main/java/com/example/kinopoisk/ui/search/SearchViewModel.kt
+++ kinopoisk Test/app/src/main/java/com/example/kinopoisk/ui/search/SearchViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.kinopoisk.ui.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
